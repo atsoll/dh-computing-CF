6f7c1005cb616a93f28eb528d91c50b3415d866f
atsoll <sollazzo@ualberta.ca>
2026-06-10 15:37:48 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+var prefix = ""
+
+
+//for scale
+var vh = window.innerHeight/100;
+var vw = window.innerWidth/100;
+
+
+
+
+var app = angular.module('template',['ngRoute', 'ngSanitize', 'ngAnimate', 'ui.bootstrap', 'duScroll']);
+
+app.config(function($routeProvider) {
+
+    //configure the routes
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "../views/overview.html"
+    }).when("/framework", {
+        templateUrl : "../views/framework.html",
+    }).when("/literature", {
+        templateUrl : "../views/literature.html",
+    })
+
+});
+
+
+
+app.controller('ctrl', function($scope, $window, $document,  $anchorScroll) {
+
+  $scope.model = {
+    active_page: 'Overview',
+    menu: [{title: "Overview", link:"#!"}, {title: "Competency Framework", link:"#!framework"}, {title: "Related Literature", link:"#!literature"}]
+  }
+
+
+
+})
